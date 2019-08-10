410394fcd9c168f39c9b9dd04a85100b08bae8f4
DT <tsengmoneymoney@gmail.com>
2019-08-11 01:51:11 +0800

    aa

--- init.R
+++ init.R
@@ -1,4 +1,24 @@
-my_packages = c("shiny", 'dplyr')
+# my_packages = c("shiny", 'dplyr')
+# 
+# install_if_missing = function(p) {
+#   if (p %in% rownames(installed.packages()) == FALSE) {
+#     install.packages(p)
+#   }
+# }
+# 
+# invisible(sapply(my_packages, install_if_missing))
+
+#
+# init.R
+# shiny-heroku
+#
+# Created by blakiseskream on 5/7/2018
+# MIT License and shit
+#
+
+my_packages = c('showtext', "shiny", 'shinythemes', 'shinyjs', 'shinydashboard',
+                'shinyWidgets', 'DT', 'plotly',
+                'magrittr', 'dplyr', 'readr', 'stringr')
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
