01c94f44fc4ade397fcbdc4a906cd49d53c85dff
DT <tsengmoneymoney@gmail.com>
2019-08-11 01:52:30 +0800

    aa

--- init.R
+++ init.R
@@ -1,24 +1,4 @@
-# my_packages = c("shiny", 'dplyr')
-# 
-# install_if_missing = function(p) {
-#   if (p %in% rownames(installed.packages()) == FALSE) {
-#     install.packages(p)
-#   }
-# }
-# 
-# invisible(sapply(my_packages, install_if_missing))
-
-#
-# init.R
-# shiny-heroku
-#
-# Created by blakiseskream on 5/7/2018
-# MIT License and shit
-#
-
-my_packages = c('showtext', "shiny", 'shinythemes', 'shinyjs', 'shinydashboard',
-                'shinyWidgets', 'DT', 'plotly',
-                'magrittr', 'dplyr', 'readr', 'stringr')
+my_packages = c("shiny", 'dplyr')
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
@@ -27,3 +7,23 @@ install_if_missing = function(p) {
 }
 
 invisible(sapply(my_packages, install_if_missing))
+
+#
+# init.R
+# shiny-heroku
+#
+# Created by blakiseskream on 5/7/2018
+# MIT License and shit
+#
+# 
+# my_packages = c('showtext', "shiny", 'shinythemes', 'shinyjs', 'shinydashboard',
+#                 'shinyWidgets', 'DT', 'plotly',
+#                 'magrittr', 'dplyr', 'readr', 'stringr')
+# 
+# install_if_missing = function(p) {
+#   if (p %in% rownames(installed.packages()) == FALSE) {
+#     install.packages(p)
+#   }
+# }
+# 
+# invisible(sapply(my_packages, install_if_missing))
