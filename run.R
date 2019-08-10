f7f4709071e866eb293a8988678456930a16c846
DT <tsengmoneymoney@gmail.com>
2019-08-11 01:28:14 +0800

    gg

--- run.R
+++ run.R
@@ -1,16 +1,25 @@
-# obtain port argument (if given)
-args <- commandArgs(trailingOnly = TRUE)
+# # obtain port argument (if given)
+# args <- commandArgs(trailingOnly = TRUE)
+# 
+# if (length(args) == 0) {
+# 	port <- "12000"
+# } else if (length(args) == 1) {
+#   port <- args[1]
+# }
+# 
+# print(paste("Listening on port", port))
+# 
+# shiny::runApp(
+#   appDir = getwd(),
+#   host = "127.0.0.1",
+#   port = as.numeric(port)
+# )
+library(shiny)
 
-if (length(args) == 0) {
-	port <- "12000"
-} else if (length(args) == 1) {
-  port <- args[1]
-}
-
-print(paste("Listening on port", port))
+port <- Sys.getenv('PORT')
 
 shiny::runApp(
   appDir = getwd(),
-  host = "127.0.0.1",
+  host = '0.0.0.0',
   port = as.numeric(port)
 )
